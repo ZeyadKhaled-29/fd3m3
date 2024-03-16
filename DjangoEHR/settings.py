c8c97826d1ffa385752ca52528d87af79625e26d
zeyadkhaled29 <z.badry19@gmail.com>
2024-03-16 18:55:07 +0200

    Update settings.py

--- DjangoEHR/settings.py
+++ DjangoEHR/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'DjangoEHR.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'database2',
         'USER': 'zeyad',
         'PASSWORD': 'Zeyad!12345',
         'HOST': 'mydatabase2.postgres.database.azure.com',
